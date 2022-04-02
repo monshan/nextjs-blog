e1fd2901326fefce4248244805b0bda797033596
Marika <marikashanahan@gmail.com>
2022-04-02 15:26:54 -0600

    Refactor pokemon dynamic routing

--- pages/[pokemon].js
+++ pages/[pokemon].js
@@ -3,26 +3,36 @@ import Image from 'next/image'
 import Layout from '../components/layout'
 import {useRouter} from 'next/router'
 
-const Pokemon = () => {
-    const [sprite, setSprite] = useState('')
+const Pokemon = (props) => {
+    // const [sprite, setSprite] = useState('')
     const userQuery = useRouter().query.pokemon
 
-    const fetchInfo = async () => {
-        const res = await fetch('https://pokeapi.co/api/v2/pokemon/' + userQuery)
-        const data = await res.json()
-        setSprite(data.sprites.front_default)
-    }
+    // const fetchInfo = async () => {
+    //     const res = await fetch('https://pokeapi.co/api/v2/pokemon/' + userQuery)
+    //     const data = await res.json()
+    //     setSprite(data.sprites.front_default)
+    // }
 
-    useEffect(() => {
-       fetchInfo()
-    })
+    // useEffect(() => {
+    //    fetchInfo()
+    // })
 
     return (
         <Layout>
-            <h1>Here's your {userQuery}</h1>
-            {sprite && <img src={sprite}/>}
+            <h1>Here's your {props.userQuery}</h1>
+            {sprite && <img src={props.sprite}/>}
         </Layout>
     ) 
+}
+
+export const getStaticProps = async () => {
+    const userQuery = useRouter().query.pokemon
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon/' + userQuery)
+    const data = await res.json()
+    return {
+        sprite: data.sprites.front_default,
+        userQuerey: userQuery
+    }
 }
 
 export default Pokemon
